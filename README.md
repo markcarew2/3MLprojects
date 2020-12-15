6257a99e1327271cfb9461819b11881fbca5011e
Mark Carew <markycarew@hotmail.com>
2020-12-15 13:17:47 -0500

    Better Graphing

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 #Implementation of Perceptron Learning Algorithm, gradient descent algorithm for linear regression, and various Machine Learning tools from SKLearn
+
+skLearnGridSearch takes a while to run, its grid searching a range of parameters for 7 classifiers:
+
+    3 Support vector machines, linear, polynomial, and RBF kernels
+    K-Nearest Neighbours Classifier
+    Logistic Regression Classifier
+    Random Forest Classifier
+    Decision Tree Classifier
--- sklearnGridSearch.py
+++ sklearnGridSearch.py
@@ -15,13 +15,14 @@ trainDF, testDF = train_test_split(df, train_size = .6, stratify = df["label"], 
 #kf = KFold()
 #splitData = kf.split(trainDF)
 
+#Make the classifiers of different types
 svr = svm.SVC()
 knn = neighbors.KNeighborsClassifier()
 lgrg = linear_model.LogisticRegression()
 dTree = tree.DecisionTreeClassifier()
 rForest = ensemble.RandomForestClassifier()
 
-
+#Set up the various gridsearches we're going to do
 linearClassifier = GridSearchCV(svr, {'kernel': ['linear'], 'C':[0.1, 0.5, 1, 5, 10, 50, 100]})
 polyClassifier = GridSearchCV(svr, {'kernel': ['poly'], 'C':[0.1, 1, 3], 'degree':[4, 5, 6],'gamma':[0.1,0.5]})
 rbfClassifier = GridSearchCV(svr, {'kernel':['rbf'], 'C':[0.1, 0.5, 1, 5, 10, 50, 100], 'gamma':[0.1, 0.5, 1, 3, 6, 10]})
@@ -30,81 +31,179 @@ lgrgClassifier = GridSearchCV(lgrg, {'C': [0.1, 0.5, 1, 5, 10, 50, 100]})
 rFClassifier = GridSearchCV(rForest, {'max_depth': [a+1 for a in range(50)], 'min_samples_split': [a for a in range(2,11)]})
 dTreeClassifier = GridSearchCV(dTree, {'max_depth': [a+1 for a in range(50)], 'min_samples_split': [a for a in range(2,11)]})
 
-linearClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(linearClassifier.cv_results_)
-print(linearClassifier.best_estimator_)
-bestLinScore = linearClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(bestLinScore)
-linTest = linearClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
-
-
-polyClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(polyClassifier.cv_results_)
-print(polyClassifier.best_estimator_)
-bestPolyScore = polyClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(bestPolyScore)
-polyScore = polyClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
-
-rbfClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(rbfClassifier.cv_results_)
-print(rbfClassifier.best_estimator_)
-bestrbfScore = rbfClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(bestrbfScore)
-rbfTest = rbfClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
-print(rbfTest)
-
-knnClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(knnClassifier.cv_results_)
-print(knnClassifier.best_estimator_)
-bestknnScore = knnClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(bestknnScore)
-knnTest = knnClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
-print(knnTest)
-
-lgrgClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(lgrgClassifier.cv_results_)
-print(lgrgClassifier.best_estimator_)
-bestlgrgScore = lgrgClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(bestlgrgScore)
-lgrgTest = lgrgClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
-
-rFClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(rFClassifier.cv_results_)
-print(rFClassifier.best_estimator_)
-bestrFScore = rFClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(bestrFScore)
-rFTest = rFClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
-
-dTreeClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(dTreeClassifier.cv_results_)
-print(dTreeClassifier.best_estimator_)
-bestdTreeScore = dTreeClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
-print(bestdTreeScore)
-dTreeTest = dTreeClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
-
+#Set up variables for graphing
+#We'll produce graphs after every classifier 
+#and a summary graph at the end
 x_min, x_max = testDF.iloc[:,0].min() - 1, testDF.iloc[:,0].max() + 1
 y_min, y_max = testDF.iloc[:,1].min() - 1, testDF.iloc[:,1].max() + 1
-
 xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
                      np.arange(y_min, y_max, 0.1))
 
-fig, ax = plt.subplots(4,2, sharex="col", sharey='row', figsize = (14,8))
+
+#Train the Linear Classifier
+linearClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+#print(linearClassifier.cv_results_)
+print("Best Parameters: ", linearClassifier.best_estimator_)
+bestLinScore = linearClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+print("SVM Linear Classifier Train Score: ", bestLinScore)
+linTest = linearClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
+print("Test Score: ", linTest)
+
+#Graph the Linear Classifier with Test Data
+Z = linearClassifier.predict(np.c_[xx.ravel(),yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, alpha=.4)
+plt.scatter(testDF.iloc[:,0], testDF.iloc[:,1],c=testDF.iloc[:,2])
+title = linearClassifier.best_estimator_
+plt.title(title)
+scores = "Test Score: " + str(linTest)
+plt.xlabel(scores)
+plt.show()
+
+
+#Repeat With other classifiers
+
+#Train Poly
+polyClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+#print(polyClassifier.cv_results_)
+print("\nBest Parameters: ", polyClassifier.best_estimator_)
+bestPolyScore = polyClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+print("Polynomial SVM Train Score: ", bestPolyScore)
+polyScore = polyClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
+print("Test Score: ", polyScore)
+
+#Graph Poly
+Z = polyClassifier.predict(np.c_[xx.ravel(),yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, alpha=.4)
+plt.scatter(testDF.iloc[:,0], testDF.iloc[:,1],c=testDF.iloc[:,2])
+title = polyClassifier.best_estimator_
+plt.title(title)
+scores = "Test Score: " + str(polyScore)
+plt.xlabel(scores)
+plt.show()
+
+#Train RBF
+rbfClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+#print(rbfClassifier.cv_results_)
+print("\nBest Parameters: ", rbfClassifier.best_estimator_)
+bestrbfScore = rbfClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+print("RBF SVM Train Score: ", bestrbfScore)
+rbfTest = rbfClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
+print("Test Score: ", rbfTest)
+
+#Graph RBF
+Z = rbfClassifier.predict(np.c_[xx.ravel(),yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, alpha=.4)
+plt.scatter(testDF.iloc[:,0], testDF.iloc[:,1],c=testDF.iloc[:,2])
+title = rbfClassifier.best_estimator_
+plt.title(title)
+scores = "Test Score: " + str(rbfTest)
+plt.xlabel(scores)
+plt.show()
+
+#Train KNN Classifier
+knnClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+#print(knnClassifier.cv_results_)
+print("\nBest Parameters: ", knnClassifier.best_estimator_)
+bestknnScore = knnClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+print("K-Nearest Neighbours Train Score: ",bestknnScore)
+knnTest = knnClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
+print("Test Score: ", knnTest)
+
+#Graph KNN Classifier
+Z = knnClassifier.predict(np.c_[xx.ravel(),yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, alpha=.4)
+plt.scatter(testDF.iloc[:,0], testDF.iloc[:,1],c=testDF.iloc[:,2])
+title = knnClassifier.best_estimator_
+plt.title(title)
+scores = "Test Score: " + str(knnTest)
+plt.xlabel(scores)
+plt.show()
+
+#Train Logistic Regression Classifier
+lgrgClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+#print(lgrgClassifier.cv_results_)
+print("\nBest Parameters: ", lgrgClassifier.best_estimator_)
+bestlgrgScore = lgrgClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+print("Logistic Regression Train Score: ", bestlgrgScore)
+lgrgTest = lgrgClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
+print("Test Score: ", lgrgTest)
+
+#Graph Logistic regression
+Z = lgrgClassifier.predict(np.c_[xx.ravel(),yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, alpha=.4)
+plt.scatter(testDF.iloc[:,0], testDF.iloc[:,1],c=testDF.iloc[:,2])
+title = lgrgClassifier.best_estimator_
+plt.title(title)
+scores = "Test Score: " + str(lgrgTest)
+plt.xlabel(scores)
+plt.show()
+
+#Train Random Forest Classifier
+rFClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+#print(rFClassifier.cv_results_)
+print("\nBest Parameters: ", rFClassifier.best_estimator_)
+bestrFScore = rFClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+print("Random Forest Train Score: ",bestrFScore)
+rFTest = rFClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
+print("Test Score: ", rFTest)
+
+#Graph Random Forest
+Z = rFClassifier.predict(np.c_[xx.ravel(),yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, alpha=.4)
+plt.scatter(testDF.iloc[:,0], testDF.iloc[:,1],c=testDF.iloc[:,2])
+title = rFClassifier.best_estimator_
+plt.title(title)
+scores = "Test Score: " + str(rFTest)
+plt.xlabel(scores)
+plt.show()
+
+#Train Decision Tree Classifier
+dTreeClassifier.fit(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+#print(dTreeClassifier.cv_results_)
+print("\nBest Parameters: ", dTreeClassifier.best_estimator_)
+bestdTreeScore = dTreeClassifier.score(trainDF.iloc[:,[0,1]], trainDF.iloc[:,2])
+print("Decision Tree Train Score: ",bestdTreeScore)
+dTreeTest = dTreeClassifier.score(testDF.iloc[:,[0,1]], testDF.iloc[:,2])
+print("Test Score: ", dTreeTest)
+
+#Graph Decision Tree Classifier
+Z = dTreeClassifier.predict(np.c_[xx.ravel(),yy.ravel()])
+Z = Z.reshape(xx.shape)
+plt.contourf(xx, yy, Z, alpha=.4)
+plt.scatter(testDF.iloc[:,0], testDF.iloc[:,1],c=testDF.iloc[:,2])
+title = dTreeClassifier.best_estimator_
+plt.title(title)
+scores = "Test Score: " + str(dTreeTest)
+plt.xlabel(scores)
+plt.show()
+
+
+#Set up the summary graph
+fig, ax = plt.subplots(4,2, sharex="col", sharey='row', figsize = (18,12))
+plt.subplots_adjust(hspace=.4)
 
 prod = product([0,1,2,3],[0,1])
 
 classifiers = [dTreeClassifier, rFClassifier,lgrgClassifier,knnClassifier,rbfClassifier,polyClassifier,linearClassifier]
 titles = ["Decision Tree", "Random Forest", "Logistic R", "KNN", "RBF", "Polynomial", "Linear"]
+testScores = [dTreeTest, rFTest, lgrgTest, knnTest, rbfTest, polyScore, linTest]
 
-for idx, clf, tt in zip(prod, classifiers, titles):
+#Populate the Subplots
+for idx, clf, tt, score in zip(prod, classifiers, titles, testScores):
 
-    print(idx)
-    print(clf)
-    print(tt)
     Z = clf.predict(np.c_[xx.ravel(),yy.ravel()])
     Z = Z.reshape(xx.shape)
 
     ax[idx[0],idx[1]].contourf(xx, yy, Z, alpha=.4)
     ax[idx[0],idx[1]].scatter(testDF.iloc[:,0], testDF.iloc[:,1],c=testDF.iloc[:,2])
     ax[idx[0],idx[1]].set_title(tt)
+    scores = "Test Score: " + str(score)
+    ax[idx[0],idx[1]].set_xlabel(scores)
 
 plt.show()
